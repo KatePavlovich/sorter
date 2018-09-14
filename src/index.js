5749fe62c23a37e11c04782d4d85df8a12e7275e
KatePavlovich <asylumhere@mail.ru>
2018-09-14 13:42:49 +0300

    partial solution

--- src/index.js
+++ src/index.js
@@ -1,26 +1,56 @@
 class Sorter {
   constructor() {
     // your implementation
+    this.arr = [];
   }
 
   add(element) {
     // your implementation
+    this.arr.push(element);
   }
 
   at(index) {
     // your implementation
+    return this.arr[index];
   }
 
   get length() {
     // your implementation
+    return this.arr.length;
   }
 
   toArray() {
     // your implementation
+    return [...this.arr]
   }
 
   sort(indices) {
     // your implementation
+    let newArr = [];
+    for (let i = 0; i < this.arr.length; i++) {
+      for (let j = 0; j < indices.length; j++) {
+        if (i === indices[j]) {
+          newArr.push(this.arr[i])
+        }
+      }
+    }
+    //if (indices.length>1) {
+    if (indices[0] < indices[1]) {
+      newArr.sort((a, b) => a - b);
+    } else {
+      newArr.sort((a, b) => b - a);
+    }
+  //}
+
+    for (let i = 0; i < this.arr.length; i++) {
+      for (let j = 0; j < indices.length; j++) {
+        if (i === indices[j]) {
+          this.arr[i] = newArr[j]
+        }
+      }
+    }
+  
+    return this.arr;
   }
 
   setComparator(compareFunction) {
